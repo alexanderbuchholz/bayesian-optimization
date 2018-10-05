b8063bf85d99f1986ef8bc49b0a09d5c95b18d81
buchhola <alexander.buchholz@ensae.fr>
2018-10-05 12:36:56 +0200

    add one expected improvement

--- gpytorch/gp_regression_by_hand.py
+++ gpytorch/gp_regression_by_hand.py
@@ -3,6 +3,8 @@
 import math
 import torch
 import gpytorch
+import matplotlib
+matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
 
@@ -15,44 +17,47 @@ mkernel = MaternKernel()
 
 X_to_evaluate = torch.ones(5, 1, requires_grad=True)
 X_rand = torch.rand(5, 1, requires_grad=False)
-#x_test = torch.linspace(0,4,20, requires_grad=False).unsqueeze(1)
+#x_train = torch.linspace(0,4,20, requires_grad=False).unsqueeze(1)
 
-x_test = x_end*torch.rand(20, 1, requires_grad=False)#.unsqueeze(1)
-y_test = torch.sin(x_test)#+torch.normal(torch.ones(x_test.shape))
+x_train = x_end*torch.rand(20, 1, requires_grad=False)#.unsqueeze(1)
+y_train = torch.sin(x_train)#+torch.normal(torch.ones(x_train.shape))
 
-def mean_pred(X_to_evaluate, y_test, x_test):
-    #mu_zeros = torch.zeros(y_test.shape)
-    mu_prior = torch.zeros(y_test.shape)
-    K_test = mkernel.forward(x_test, x_test).squeeze(0)+sigma_prior*torch.eye(x_test.shape[0])
-    K_new = mkernel.forward(X_to_evaluate, x_test).squeeze(0)
-    mu_predict = torch.matmul(K_new, torch.inverse(K_test)).mm(y_test-mu_prior)
+def mean_pred(X_to_evaluate, y_train, x_train):
+    #mu_zeros = torch.zeros(y_train.shape)
+    mu_prior = torch.zeros(y_train.shape)
+    K_test = mkernel.forward(x_train, x_train).squeeze(0)+sigma_prior*torch.eye(x_train.shape[0])
+    K_new = mkernel.forward(X_to_evaluate, x_train).squeeze(0)
+    mu_predict = torch.matmul(K_new, torch.inverse(K_test)).mm(y_train-mu_prior)
     return mu_predict
 
-def covar_pred(X_to_evaluate, y_test, x_test):
-    #mu_zeros = torch.zeros(y_test.shape)
+def covar_pred(X_to_evaluate, y_train, x_train):
+    #mu_zeros = torch.zeros(y_train.shape)
     
-    K_test = mkernel.forward(x_test, x_test).squeeze(0)+sigma_prior*torch.eye(x_test.shape[0])
-    K_new = mkernel.forward(X_to_evaluate, x_test).squeeze(0)
+    K_test = mkernel.forward(x_train, x_train).squeeze(0)+sigma_prior*torch.eye(x_train.shape[0])
+    K_new = mkernel.forward(X_to_evaluate, x_train).squeeze(0)
     K_pred = mkernel.forward(X_to_evaluate, X_to_evaluate).squeeze(0)
     sigma_predict = K_pred - torch.matmul(K_new, torch.inverse(K_test)).mm(K_new.t())
     return sigma_predict
 
-def expected_improvement(X_to_evaluate, y_test, x_test, sample_size=100):
-    f_max = y_test.max()
-    out_covar = covar_pred(X_to_evaluate, y_test, x_test)
-    out_mean = mean_pred(X_to_evaluate, y_test, x_test)
+def one_expected_improvement(X_to_evaluate, y_train, x_train, sample_size=100):
+    f_max = y_train.max()
+    out_covar = covar_pred(X_to_evaluate, y_train, x_train)
+    out_mean = mean_pred(X_to_evaluate, y_train, x_train)
     L_x = torch.potrf(out_covar, upper=False)
     Z = torch.normal(torch.ones(X_to_evaluate.shape[0], sample_size))
-    inner_term = out_mean + L_x.mm(Z)
+    inner_term = torch.max(out_mean + L_x.mm(Z), torch.zeros(out_mean.shape))
+    import ipdb; ipdb.set_trace()
+    return inner_term.mean(1)
 
+X_one = torch.ones(1, 1, requires_grad=True)
 
+mean_expected_improvement = one_expected_improvement(X_one, y_train, x_train)
 
-
-#out_covar = covar_pred(X_to_evaluate, y_test, x_test)
-#out_mean = mean_pred(X_to_evaluate, y_test, x_test)
+#out_covar = covar_pred(X_to_evaluate, y_train, x_train)
+#out_mean = mean_pred(X_to_evaluate, y_train, x_train)
 if False: 
     for t in range(1000):
-        out_mean = mean_pred(X_to_evaluate, y_test, x_test)
+        out_mean = mean_pred(X_to_evaluate, y_train, x_train)
         out_mean.backward(X_rand)
         with torch.no_grad():
             X_to_evaluate += 0.001 * X_to_evaluate.grad
@@ -60,13 +65,16 @@ if False:
 # play around and make some predictions
 if True: 
     x_support = torch.linspace(0, x_end, 100, requires_grad=False).unsqueeze(1)
-    confidence_bound = covar_pred(x_support, y_test, x_test).diag()
-    predicted_y = mean_pred(x_support, y_test, x_test).squeeze()
-    plt.scatter(x_test.data.numpy(), y_test.data.numpy(), label='true points')
+    confidence_bound = covar_pred(x_support, y_train, x_train).diag()
+    predicted_y = mean_pred(x_support, y_train, x_train).squeeze()
+    plt.scatter(x_train.data.numpy(), y_train.data.numpy(), label='true points')
     plt.plot(x_support.data.numpy(), predicted_y.data.numpy(), label='predicted_points')
     plt.plot(x_support.data.numpy(), (predicted_y+2*confidence_bound).data.numpy(), label='upper bound')
     plt.plot(x_support.data.numpy(), (predicted_y-2*confidence_bound).data.numpy(), label='lower bound')
+    plt.plot(x_support.data.numpy(), one_expected_improvement(x_support, y_train, x_train).data.numpy(), label='expected improvement')
     plt.legend()
     plt.show()
+
+
 
 import ipdb; ipdb.set_trace()
