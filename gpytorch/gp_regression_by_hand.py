8cd2fc57f25f07afcc0bfa813a3ab2d47474b525
Alex <alexander.buchholz@ensae.fr>
2018-10-22 23:01:32 +0200

    try to fix error with q exepcted improvement

--- gpytorch/gp_regression_by_hand.py
+++ gpytorch/gp_regression_by_hand.py
@@ -25,14 +25,18 @@ y_train = torch.sin(torch.norm(x_train, dim=1))#+torch.normal(torch.ones(x_train
 #import ipdb; ipdb.set_trace()
 
 def mean_pred(X_to_evaluate, y_train, x_train):
+    assert X_to_evaluate.shape[1] == x_train.shape[1]
     #mu_zeros = torch.zeros(y_train.shape)
     mu_prior = torch.zeros(y_train.shape)
+    #import ipdb; ipdb.set_trace()
     K_test = mkernel.forward(x_train, x_train).squeeze(0)+sigma_prior*torch.eye(x_train.shape[0])
     K_new = mkernel.forward(X_to_evaluate, x_train).squeeze(0)
-    mu_predict = torch.matmul(K_new, torch.inverse(K_test)).mm(y_train-mu_prior)
+    #import ipdb; ipdb.set_trace()
+    mu_predict = torch.matmul(K_new, torch.inverse(K_test)).mm((y_train-mu_prior).unsqueeze(1))
     return mu_predict
 
 def covar_pred(X_to_evaluate, y_train, x_train):
+    assert X_to_evaluate.shape[1] == x_train.shape[1]
     #mu_zeros = torch.zeros(y_train.shape)
     
     K_test = mkernel.forward(x_train, x_train).squeeze(0)+sigma_prior*torch.eye(x_train.shape[0])
@@ -42,15 +46,18 @@ def covar_pred(X_to_evaluate, y_train, x_train):
     return sigma_predict
 
 def one_expected_improvement(X_to_evaluate, y_train, x_train, sample_size=100):
+    assert X_to_evaluate.shape[1] == x_train.shape[1]
     #import ipdb; ipdb.set_trace()
     f_max = y_train.max()
     out_covar = covar_pred(X_to_evaluate, y_train, x_train)
+    #import ipdb; ipdb.set_trace()
     out_mean = mean_pred(X_to_evaluate, y_train, x_train)
     L_x = torch.potrf(out_covar, upper=False)
     Z = torch.normal(torch.ones(X_to_evaluate.shape[0], sample_size))
-    inner_term = torch.max(f_max - (out_mean - L_x.mm(Z)), torch.zeros(out_mean.shape))
+    min_value, __ = torch.min(out_mean + L_x.mm(Z), dim=0)
+    inner_term = torch.max((f_max - min_value), torch.zeros(sample_size))
     #import ipdb; ipdb.set_trace()
-    return inner_term.mean(1)
+    return inner_term.mean()
 
 #X_one = torch.ones(1, 1, requires_grad=True)
 
@@ -60,8 +67,9 @@ def one_expected_improvement(X_to_evaluate, y_train, x_train, sample_size=100):
 #out_mean = mean_pred(X_to_evaluate, y_train, x_train)
 if False: 
     for t in range(1000):
+        import ipdb; ipdb.set_trace()
         out_mean = mean_pred(X_to_evaluate, y_train, x_train)
-        out_mean.backward(X_rand)
+        out_mean.backward(y_train)
         with torch.no_grad():
             X_to_evaluate += 0.001 * X_to_evaluate.grad
             X_to_evaluate.grad.zero_()
@@ -69,7 +77,7 @@ if False:
 m_multistart = 10
 import copy
 import numpy as np
-X_test = (torch.rand((m_multistart, 1), requires_grad=False)*2*x_end)-x_end
+X_test = (torch.rand((m_multistart, dim_x), requires_grad=False)*2*x_end)-x_end
 X_test_copy = copy.deepcopy(X_test)
 #import ipdb; ipdb.set_trace()
 #X_test = torch.tensor([9.], requires_grad=True, dtype=torch.float)
@@ -78,10 +86,12 @@ multistart_x_test = []
 all_points_list = []
 all_loss_list = []
 for i_multistart in range(m_multistart):
-    X_test_inter = X_test[i_multistart,:]
+    X_test_inter = X_test[i_multistart,:].unsqueeze(0)
     X_test_inter.requires_grad = True
     #import ipdb; ipdb.set_trace()
     for t in range(1000):
+        import ipdb; ipdb.set_trace()
+        one_expected_improvement(X_test, y_train, x_train, 1000)
         loss = one_expected_improvement(X_test_inter, y_train, x_train, 1000)
         all_loss_list.append(loss.data.numpy())
         all_points_list.append(copy.deepcopy(X_test_inter).data.numpy())
